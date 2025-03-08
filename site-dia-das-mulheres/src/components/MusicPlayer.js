468d57f0983da8c87e68f46a9954e48d09a74e1f
Matheus kauan pinto <mkauanpinto@gmail.com>
2025-03-08 22:49:23 +0000

    dfgfdg

--- site-dia-das-mulheres/src/components/MusicPlayer.js
+++ site-dia-das-mulheres/src/components/MusicPlayer.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const MusicPlayer = () => {
+  useEffect(() => {
+    const iframe = document.querySelector("iframe");
+    // Se o autoplay não funcionar, há uma alternativa que força a música a tocar
+    iframe.src += "&autoplay=true";
+  }, []);
+
   return (
     <div className="music-container">
       <iframe
         title="Spotify Player"
         style={{ borderRadius: "12px" }}
-        src="https://open.spotify.com/embed/track/70HrtYu3IBWpBIrPLEkcuP?utm_source=generator&autoplay=true" // Parâmetro autoplay=true
-        width="300" // Ajuste a largura
-        height="80" // Ajuste a altura
+        src="https://open.spotify.com/embed/track/70HrtYu3IBWpBIrPLEkcuP?utm_source=generator"
+        width="100%"
+        height="80" // Ajustando o tamanho do player
         frameBorder="0"
-        allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
